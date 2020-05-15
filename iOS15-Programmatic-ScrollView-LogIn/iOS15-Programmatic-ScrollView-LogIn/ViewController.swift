d810be2c40fdd9d1629d50c1523dfbcb903f6955
Paul Solt <PaulSolt@gmail.com>
2020-05-15 12:37:08 -0400

    Added scroll view with a content view (size will be determined by height of content)

--- iOS15-Programmatic-ScrollView-LogIn/iOS15-Programmatic-ScrollView-LogIn/ViewController.swift
+++ iOS15-Programmatic-ScrollView-LogIn/iOS15-Programmatic-ScrollView-LogIn/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let imageHeight: CGFloat = 300
+    let buttonHeight: CGFloat = 60
+    let textFieldHeight: CGFloat = 40
+    
+    // Lazy properties (future extract to custom classes to reuse among screens)
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView(frame: .zero)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .cyan
+        return scrollView
+    }()
+    
+    lazy var contentView: UIView = {
+        let contentView = UIView()
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.backgroundColor = .systemPink
+        return contentView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        addScrollView()
     }
+    
+    private func addScrollView() {
+        view.addSubview(scrollView)
+        
+        scrollView.addSubview(contentView)
+        
+        view.addConstraints([
+            // superview (Storyboard) = view
+            
+            // Safe area = under the time
+            // Scroll View Constraints
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            // Content View Constraints (Prevents content from being squished to intrinsic content size)
+            contentView.widthAnchor.constraint(equalTo: view.widthAnchor),
+//            contentView.heightAnchor.constraint(equalTo: view.heightAnchor),
 
-
+            
+        ])
+    }
 }
 
