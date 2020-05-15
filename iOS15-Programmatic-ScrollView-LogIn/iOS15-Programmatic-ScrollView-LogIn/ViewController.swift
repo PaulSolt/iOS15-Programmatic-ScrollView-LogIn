2f14bff2615d785f10882b72fb6ff03a9ad114a8
Paul Solt <PaulSolt@gmail.com>
2020-05-15 12:19:39 -0400

    Starter project with images

---
+++ iOS15-Programmatic-ScrollView-LogIn/iOS15-Programmatic-ScrollView-LogIn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS15-Programmatic-ScrollView-LogIn
+//
+//  Created by Paul Solt on 5/15/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
